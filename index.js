c1a1fdb7e75acde25e690acb7e28e67f07b91939
Rezahaji1368 <Reza.haji1368@gmail.com>
2023-06-21 10:29:08 +0330

    first git page

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-console.log("bill");
-const p=3.14;
-console.log(Math.floor(p));
+const cal=document.querySelector(".calculate")
+cal.addEventListener("click",calPi)
+function calPi(){
+    const p=3.14;
+    console.log(Math.floor(p));
+    document.querySelector(".result").innerHTML=p
+}
